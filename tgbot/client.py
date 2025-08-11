ae015b30b74c68138e1485e633e2bdcdecbce876
maxim <progrm16@gmail.com>
2025-08-11 14:40:27 +0300

    fix

--- tgbot/client.py
+++ tgbot/client.py
@@ -1,15 +1,3 @@
-import pandas as pd
-df = pd.read_parquet('../math_problems.parquet', engine='pyarrow')
-# Отключаем ограничения на количество отображаемых столбцов и строк
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-
-# Убираем ограничение ширины колонок
-pd.set_option('display.width', None)
-
-# Полностью показываем содержимое каждой ячейки
-pd.set_option('display.max_colwidth', None)
-with open('output.txt', 'w') as f:
-    print([
-    {"code": f"01-003-3-{i}", "title": f"IMO Math - {i}"} for i in range(len(df.index))
-], file=f)
+task_code = '00-111-2-3-4_g_20250811_141607'
+task_id = task_code.split("_")[0].split("-")[-1]
+print(task_id)
